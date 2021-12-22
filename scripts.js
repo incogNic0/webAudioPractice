d728e09baa1d6bdc6266847aedca7e92e580bdbe
incogNic0 <incognico.dev@gmail.com>
2021-12-22 17:09:24 -0600

    add low/high pass filters

--- scripts.js
+++ scripts.js
@@ -1,190 +1,258 @@
-const audioContext = new AudioContext() ||  new window.webkitAudioContext();
+const audioContext = new AudioContext() || new window.webkitAudioContext();
+const filter = audioContext.createBiquadFilter();
 const lookahead = 25.0;
 const scheduleAheadTime = 0.1;
-let tempo = 90
+let tempo = 90;
 let currentStep = 0;
 let nextStepTime = 0.0; // when the next note is due.
 let timerID;
 let isPlaying = false;
+let activeFilter = false;
 let kitState = [
-    [false, false, false, false],
-    [false, false, false, false],
-    [false, false, false, false],
-    [false, false, false, false]
-]
+	[false, false, false, false],
+	[false, false, false, false],
+	[false, false, false, false],
+	[false, false, false, false],
+];
 let animationType = animateAll;
 let runAnimation;
 const audioBuffers = [];
 setupSamples(); // add buffer for each sample to audioBuffers
 
-
-// START / STOP 
-const playBtn = document.querySelector('#play-btn');
-playBtn.addEventListener('click', handlePlayEvent, false);
+// START / STOP
+const playBtn = document.querySelector("#play-btn");
+playBtn.addEventListener("click", handlePlayEvent, false);
 
 // CHANGE TEMPO
-const tempoInput = document.querySelector('#tempo');
-const tempoDisplay = document.querySelector('#current-tempo');
-tempoInput.addEventListener('input', handleTempoChange );
+const tempoInput = document.querySelector("#tempo");
+const tempoDisplay = document.querySelector("#current-tempo");
+tempoInput.addEventListener("input", handleTempoChange);
 
 // ADJUST GAIN
-const gainInputs = document.querySelectorAll('.gain-input');
-gainInputs.forEach( gain => {
-    gain.addEventListener('input', handleGainEvent );
+const gainInputs = document.querySelectorAll(".gain-input");
+gainInputs.forEach((gain) => {
+	gain.addEventListener("input", handleGainEvent);
 });
 
 // SELECT PADS
-const pads = Array.from(document.querySelectorAll('.pads'));
-pads.forEach( pad => {
-    pad.addEventListener('click', handlePadClick );
+const pads = Array.from(document.querySelectorAll(".pads"));
+pads.forEach((pad) => {
+	pad.addEventListener("click", handlePadClick);
 });
 
-// RESET PADS 
-const resetBtn = document.querySelector('#reset-btn');
-resetBtn.addEventListener('click', handleReset);
+// RESET PADS
+const resetBtn = document.querySelector("#reset-btn");
+resetBtn.addEventListener("click", handleReset);
 
 // ANIMATION SELECTOR
-const animationInput = document.querySelector('#animation-input');
-animationInput.addEventListener('change', handleAnimationOption );
+const animationInput = document.querySelector("#animation-input");
+animationInput.addEventListener("change", handleAnimationOption);
 
+// FILTERS SELECTION
+const lpFilter = document.querySelector('#low-pass'); // low pass
+const hpFilter = document.querySelector('#high-pass') // high pass
+lpFilter.addEventListener('click', handleFilterClick);
+hpFilter.addEventListener('click', handleFilterClick);
+
+// FILTER INPUTS
+const filterColor = document.querySelector('#filter-color');
+const filterCutoff = document.querySelector('#filter-cutoff');
+filterColor.addEventListener('input', handleFilterInput);
+filterCutoff.addEventListener('input', handleFilterInput);
 
 // ========== EVENT HANDLERS ============
 // START / STOP
 async function handlePlayEvent() {
-    if(audioContext.state === 'suspended') audioContext.resume();
-    isPlaying = !isPlaying
+	if (audioContext.state === "suspended") audioContext.resume();
+	isPlaying = !isPlaying;
 
-    if(isPlaying) {
-        playBtn.textContent = 'Stop'
-        currentStep = 0;
-        nextStepTime = audioContext.currentTime;
-        scheduler(); // handles scheduling and playback
-        runAnimation = requestAnimationFrame(animationType);
-    } else {
-        playBtn.textContent = 'Start'
-        clearTimeout(timerID);
-        audioContext.currentTime = 0;
-        clearAnimation();
-        cancelAnimationFrame(runAnimation);
-    }
+	if (isPlaying) {
+		playBtn.textContent = "Stop";
+		currentStep = 0;
+		nextStepTime = audioContext.currentTime;
+		scheduler(); // handles scheduling and playback
+		runAnimation = requestAnimationFrame(animationType);
+	} else {
+		playBtn.textContent = "Start";
+		clearTimeout(timerID);
+		audioContext.currentTime = 0;
+		clearAnimation();
+		cancelAnimationFrame(runAnimation);
+	}
 }
 
 // CHANGE TEMPO
 function handleTempoChange(evt) {
-    const newTempo = evt.target.value;
-    tempoDisplay.textContent = newTempo;
-    tempo = newTempo;
+	const newTempo = evt.target.value;
+	tempoDisplay.textContent = newTempo;
+	tempo = newTempo;
 }
 
 // RESET
 function handleReset() {
-    const selectedPads = Array.from(document.querySelectorAll('.selected'));
-    selectedPads.forEach( pad => pad.classList.remove('selected'));
-    if(isPlaying) playBtn.dispatchEvent(new Event('click'));
-    kitState = [
-        [false, false, false, false],
-        [false, false, false, false],
-        [false, false, false, false],
-        [false, false, false, false]
-    ]
-    tempo = 90;
-    tempoInput.value = tempo;
-    tempoDisplay.textContent = tempo;
-    gainInputs.forEach(input => {
-        const gainDisplay = input.parentElement.children[0].children[0]
-        gainDisplay.textContent = 1;
-        input.value = 1;
-    });
+	const selectedPads = Array.from(document.querySelectorAll(".selected"));
+	selectedPads.forEach((pad) => pad.classList.remove("selected"));
+	if (isPlaying) playBtn.dispatchEvent(new Event("click"));
+	kitState = [
+		[false, false, false, false],
+		[false, false, false, false],
+		[false, false, false, false],
+		[false, false, false, false],
+	];
+	tempo = 90;
+	tempoInput.value = tempo;
+	tempoDisplay.textContent = tempo;
+	gainInputs.forEach((input) => {
+		const gainDisplay = input.parentElement.children[0].children[0];
+		gainDisplay.textContent = 1;
+		input.value = 1;
+	});
 }
 
 function handleGainEvent(evt) {
-    const gainDisplay = evt.target.parentElement.children[0].children[0]
-    gainDisplay.textContent = evt.target.value;
+	const gainDisplay = evt.target.parentElement.children[0].children[0];
+	gainDisplay.textContent = evt.target.value;
 }
 
 // SELECT PAD
 function handlePadClick(evt) {
-    const row = Number(evt.target.parentElement.id);
-    const elem = evt.target;
-    if (elem.classList.contains('pad')) {
-        elem.classList.toggle('selected');
-    }
-    const padsArrray = Array.from(pads[row].children);
-    kitState[row] = padsArrray.map( pad => pad.classList.contains('selected'));
+	const row = Number(evt.target.parentElement.id);
+	const elem = evt.target;
+	if (elem.classList.contains("pad")) {
+		elem.classList.toggle("selected");
+	}
+	const padsArrray = Array.from(pads[row].children);
+	kitState[row] = padsArrray.map((pad) => pad.classList.contains("selected"));
 }
-
 
 // ANIMATION
 function handleAnimationOption(evt) {
-    cancelAnimationFrame(runAnimation);
-    clearAnimation();
-    const checked = evt.target.checked;
-    animationType = checked ? animateActive : animateAll;
-    runAnimation = requestAnimationFrame(animationType)
+	cancelAnimationFrame(runAnimation);
+	clearAnimation();
+	const checked = evt.target.checked;
+	animationType = checked ? animateActive : animateAll;
+	runAnimation = requestAnimationFrame(animationType);
+}
+
+// FILTER BUTTONS
+function handleFilterClick(evt) {
+  const btn = evt.target;
+
+  // Turn filter on/off
+  activeFilter = !btn.classList.contains('active');
+  
+  // Set non-clicked btn to disabled if previously wasn't
+  if(btn.id === 'low-pass') {
+    hpFilter.classList.remove('active');
+    filter.type = 'lowpass'
+  } else {
+    lpFilter.classList.remove('active');
+    filter.type = 'highpass'
+  }
+
+  filter.frequency.value = adjustedCutOff(filterCutoff.value, filter.type);
+  console.log(filter.frequency.value)
+
+  btn.classList.toggle('active');
+}
+
+// FILTER INPUTS
+function handleFilterInput(evt) {
+  const value = evt.target.value;
+  if(evt.target.id === 'filter-color') {
+    filter.Q.value = Math.floor(value * 1.3);
+  } else {
+    const freqValue = adjustedCutOff(value, filter.type);
+    filter.frequency.value = freqValue;
+  }
+}
+
+function adjustedCutOff(val, filter) {
+  let adjustedVal = val;
+  if (filter === 'highpass') {
+    if(val < 45 ) adjustedVal = val * 5;
+    if(val >= 45 && val < 50 ) adjustedVal = val * 7;
+    if(val >= 50 && val < 55 ) adjustedVal = val * 10;
+    if(val >= 55 && val < 60 ) adjustedVal = val * 12;
+    if(val >= 60 && val < 65 ) adjustedVal = val * 18;
+    if(val >= 65 && val < 70 ) adjustedVal = val * 25;
+    if(val >= 70 && val < 75 ) adjustedVal = val * 32;
+    if(val >= 75 && val < 80 ) adjustedVal = val * 40
+    if(val >= 80 && val < 85 ) adjustedVal = val * 50;
+    if(val >= 85 && val < 90 ) adjustedVal = val * 60;
+    if(val >= 90 && val < 95) adjustedVal = val * 70
+    if(val >= 95) adjustedVal = val * 80;
+  }
+  if (filter === 'lowpass') {
+    adjustedVal = Math.max(Math.floor(8000 * ((100-val) / 100)), 100);
+  }
+
+  return adjustedVal;
 }
 
 
 // ========== AUDIO ====================
 
-
 async function getFile(filepath) {
-    const res = await fetch(filepath);
-    const arrayBuffer = await res.arrayBuffer();
-    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-    return audioBuffer;
+	const res = await fetch(filepath);
+	const arrayBuffer = await res.arrayBuffer();
+	const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+	return audioBuffer;
 }
 
 async function setupSamples() {
-    const audioElems = Array.from(document.querySelectorAll('audio'));
-    for (const audio of audioElems) {
-        const url = audio.getAttribute('src');
-        const buffer = await getFile(url);
-        audioBuffers.push(buffer);
-    }
+	const audioElems = Array.from(document.querySelectorAll("audio"));
+	for (const audio of audioElems) {
+		const url = audio.getAttribute("src");
+		const buffer = await getFile(url);
+		audioBuffers.push(buffer);
+	}
 }
 
 function playback(data, startTime) {
-    const playSound = audioContext.createBufferSource();
-    const gainNode = audioContext.createGain();
-    gainNode.gain.value = data.gainValue;
-    playSound.buffer = data.audio;
+	const playSound = audioContext.createBufferSource();
+	const gainNode = audioContext.createGain();
+	gainNode.gain.value = data.gainValue;
+	playSound.buffer = data.audio;
+  if( activeFilter ) {
+    playSound.connect(gainNode).connect(filter).connect(audioContext.destination);
+  }  else {
     playSound.connect(gainNode).connect(audioContext.destination);
-    playSound.start(startTime); // will play when audioContext.currentTime === startTime
+  }
+	playSound.start(startTime); // will play when audioContext.currentTime === startTime
 }
-
 
 function incrementStep() {
-    const secondsPerBeat = 60.0 / tempo / 4; // sixteenth notes
-    nextStepTime += secondsPerBeat; // when the next step should play
-    currentStep++;
-    if (currentStep === 4) currentStep = 0; // reset after 4 quarter notes
+	const secondsPerBeat = 60.0 / tempo / 4; // sixteenth notes
+	nextStepTime += secondsPerBeat; // when the next step should play
+	currentStep++;
+	if (currentStep === 4) currentStep = 0; // reset after 4 quarter notes
 }
 
-
 function scheduler() {
-    while (nextStepTime < audioContext.currentTime + scheduleAheadTime ) {
-        scheduleSamples(currentStep, nextStepTime);
-        incrementStep();
-    }
-    // continues to call scheduler every 25ms (lookahead) 
-    timerID = setTimeout(scheduler, lookahead);
+	while (nextStepTime < audioContext.currentTime + scheduleAheadTime) {
+		scheduleSamples(currentStep, nextStepTime);
+		incrementStep();
+	}
+	// continues to call scheduler every 25ms (lookahead)
+	timerID = setTimeout(scheduler, lookahead);
 }
 
 let stepsQueue = [];
 // schedule playback for all active samples in the next step sequence
 function scheduleSamples(step, startTime) {
-    stepsQueue.push( { step, startTime });
-    const len = kitState.length;
-    for (let i=0; i<len; i++) {
-        const isActive = kitState[i][step];
-        if(isActive) {
-            const params = {
-                audio: audioBuffers[i],
-                gainValue: gainInputs[i].value
-            }
-            playback(params, startTime);
-        }
-    }
+	stepsQueue.push({ step, startTime });
+	const len = kitState.length;
+	for (let i = 0; i < len; i++) {
+		const isActive = kitState[i][step];
+		if (isActive) {
+			const params = {
+				audio: audioBuffers[i],
+				gainValue: gainInputs[i].value,
+			};
+			playback(params, startTime);
+		}
+	}
 }
 
 // ========== ANIMATIONS =============
@@ -192,59 +260,56 @@ let lastStepDrawn = 3;
 
 // ANIMATE ALL PADS BOTH ACTIVE AND INACTIVE
 function animateAll() {
-    let drawStep = lastStepDrawn;
-    const currentTime = audioContext.currentTime;
+	let drawStep = lastStepDrawn;
+	const currentTime = audioContext.currentTime;
 
-    while (stepsQueue.length && stepsQueue[0].startTime < currentTime) {
-        drawStep = stepsQueue[0].step;
-        stepsQueue.shift();   // remove note from queue
-    }
+	while (stepsQueue.length && stepsQueue[0].startTime < currentTime) {
+		drawStep = stepsQueue[0].step;
+		stepsQueue.shift(); // remove note from queue
+	}
 
-    // Only draw if the step moved.
-    if (lastStepDrawn !== drawStep) {
-        pads.forEach(el => {
-        el.children[lastStepDrawn].classList.remove('playing-all');
-        el.children[drawStep].classList.add('playing-all');
-        });
+	// Only draw if the step moved.
+	if (lastStepDrawn !== drawStep) {
+		pads.forEach((el) => {
+			el.children[lastStepDrawn].classList.remove("playing-all");
+			el.children[drawStep].classList.add("playing-all");
+		});
 
-        lastStepDrawn = drawStep;
-    }
-    // set up to draw again
-    runAnimation = requestAnimationFrame(animateAll);
+		lastStepDrawn = drawStep;
+	}
+	// set up to draw again
+	runAnimation = requestAnimationFrame(animateAll);
 }
 
 // ANIMATE ONLY ACTIVE PADS
 function animateActive() {
-    let drawStep = lastStepDrawn;
-    const currentTime = audioContext.currentTime;
+	let drawStep = lastStepDrawn;
+	const currentTime = audioContext.currentTime;
 
-    while (stepsQueue.length && stepsQueue[0].startTime < currentTime) {
-        drawStep = stepsQueue[0].step;
-        stepsQueue.shift();   // remove note from queue
-    }
+	while (stepsQueue.length && stepsQueue[0].startTime < currentTime) {
+		drawStep = stepsQueue[0].step;
+		stepsQueue.shift(); // remove note from queue
+	}
 
-    // Only draw if the step moved.
-    if (lastStepDrawn !== drawStep) {
-        pads.forEach((row, index) => {
-            const isActive = kitState[index][drawStep];
-            row.children[lastStepDrawn].classList.remove('playing');
-            if (isActive) row.children[drawStep].classList.add('playing');
-        });
+	// Only draw if the step moved.
+	if (lastStepDrawn !== drawStep) {
+		pads.forEach((row, index) => {
+			const isActive = kitState[index][drawStep];
+			row.children[lastStepDrawn].classList.remove("playing");
+			if (isActive) row.children[drawStep].classList.add("playing");
+		});
 
-        lastStepDrawn = drawStep;
-    }
-    // set up to draw again
-    runAnimation = requestAnimationFrame(animateActive);
+		lastStepDrawn = drawStep;
+	}
+	// set up to draw again
+	runAnimation = requestAnimationFrame(animateActive);
 }
 
 function clearAnimation() {
-    const lastPlayedPads = Array.from(document.querySelectorAll('.playing'));
-    const lastStepPlayed = Array.from(document.querySelectorAll('.playing-all'));
-    lastPlayedPads.forEach( pad => pad.classList.remove('playing'));
-    lastStepPlayed.forEach( pad => pad.classList.remove('playing-all'))
-    stepsQueue = [];
-    lastStepDrawn = 3
+	const lastPlayedPads = Array.from(document.querySelectorAll(".playing"));
+	const lastStepPlayed = Array.from(document.querySelectorAll(".playing-all"));
+	lastPlayedPads.forEach((pad) => pad.classList.remove("playing"));
+	lastStepPlayed.forEach((pad) => pad.classList.remove("playing-all"));
+	stepsQueue = [];
+	lastStepDrawn = 3;
 }
-
-
-
